e417834f84be8d55e042009a98be84cfa1fcd3e8
suucong <sophia021210@naver.com>
2023-09-14 12:42:58 +0900

    Week02 실습코드 Commit

---
+++ Week02/src/Week02/ContentPaneEx.java
@@ -0,0 +1,30 @@
+package Week02;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ContentPaneEx extends JFrame{
+	public ContentPaneEx() {
+		setTitle("ContentPane과 JFrame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// 창을 닫을 때 어떻게 동작할 건지에 대한
+															// 기본 값을 설정해줌.
+		
+		Container cp = getContentPane();
+		
+		cp.setBackground(Color.CYAN);	// 컨텐트팬의 배경색 설정
+		cp.setLayout(new FlowLayout());	// 컨텐트팬 내 컴포넌트들의 배치방법 설정(컴포넌트 관리자 지정)
+		
+		// JButton jb = new JButton("ok");
+		// cp.add(jb);
+		cp.add(new JButton("OK"));
+		cp.add(new JButton("Cancel"));
+		cp.add(new JButton("Ignore"));
+		
+		setSize(400, 150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+}
---
+++ Week02/src/Week02/MyFrame.java
@@ -0,0 +1,16 @@
+package Week02;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame{
+	public MyFrame() {
+		setTitle("500*500 스윙 프레임 만들기");
+		setSize(100, 50);	// 프레임 크기(처음의 사이즈 지정 -> 늘리고 줄이기 가능)
+		setVisible(true);	// 프레임 출력
+	}
+	
+	// 스윙 응용 프로그램에서는 main 메소드의 기능은 최소화
+	public static void main(String[] args) {
+		MyFrame frame = new MyFrame();
+	}
+}
